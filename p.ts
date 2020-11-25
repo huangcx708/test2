2b3900a35e5c8bf5a8aa602c5843662c6cdb32cf
huangcx708 <709812030@qq.com>
2020-11-25 15:42:40 +0800

    添加新文件

---
+++ p.ts
@@ -0,0 +1,16 @@
+
+
+//修饰符 
+//public name: string;
+//private name: string; 只能在自己类里访问
+//protected name: string; 自己和子女的类都可以访问
+//readonly name: string;
+//static name: string; 类的属性和函数与实例状态无关，类.XX进行访问
+ 
+//---------------------------------------------------
+//类和接口
+/*
+接口：
+1、对对象的形状进行抽象
+2、对类的形状（属性方法）进行抽象
+ */
